44b169722077bba814009d874699ae0d197931a5
Andriy Zhyshkovych <andrew_mail_box@yahoo.com>
2021-05-26 00:48:39 +0300

    setcookie XSS fix

--- index.php
+++ index.php
@@ -114,7 +114,7 @@ $analyze_results = isset($_COOKIE[$textHash]) ? json_decode($_COOKIE[$textHash],
 // setting cookie
 if(!isset($_COOKIE[$textHash]) && !empty($text)) {
     $result = json_encode($analyze_results);
-    setcookie($textHash, $result, time() + (86400 * 30), "/" , null, null, true);
+    setcookie($textHash, $result, time() + (86400 * 30), "/" , null, true, true);
 }
 
 ?>
